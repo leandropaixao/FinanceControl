9c49fb657fd80596bd107cdfeff398c3fc98cc98
Leandro Paixao <leandro.tec.inf@gmail.com>
2024-11-29 19:32:44 -0500

    Created the first inplementation with MudBlazor

--- Program.cs
+++ Program.cs
@@ -2,12 +2,18 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FinanceControl;
 using MudBlazor.Services;
+using FinanceControl.Data.Repositories;
+using FinanceControl.Models;
+using FinanceControl.Services.Accounts;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<IRepository<Account>, AccountsRepository>();
+builder.Services.AddScoped<IAccountsService<Account>, AccountsService>();
 
 builder.Services.AddMudServices();
 
